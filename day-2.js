bc18bcfe4b9be0e7b09f1ae67717c2259603630f
Abhiramsai22 <narayandaduabhiramsai@gmail.com>
2025-02-20 21:23:27 +0530

    day-2

---
+++ day-2.js
@@ -0,0 +1,52 @@
+//Defination and comparision
+//1)what is arrow function in java script,how does its syntax differ from a traditional function exprssion?
+//A)The Function which doesn't have function name and denoted by => is known as arrow function.it's increase to readability of anonymous function
+//Arrow functions are shorter and do not have their own this context.
+
+//question-2
+var add = (a,b)=>{
+    return a+b;
+}
+console.log(add(2,3));
+
+//Question3
+var square=n => n*n;
+
+console.log(square(4));
+
+//question-4
+//A)When an arrow function takes exactly one parameter, parentheses are optional.
+// This makes the function shorter and cleaner.
+// However, if there are zero or multiple parameters, parentheses are required.Multiple parameters (must use parentheses)
+//No parameters (must use parentheses)
+
+//question-5
+// Here are the two versions of an arrow function that calculates the cube of a number:
+// 1. Concise Body (Implicit Return)
+// No {} needed.
+// The return is implicit (automatically returned).
+const cube1 = x => x * x * x;
+console.log(cube1(3));
+
+// Block Body (Explicit Return)
+// Uses {} to define a block.
+// Must use return explicitly
+const cube2 = (x) => {
+    return x * x * x;
+  };
+console.log(cube2(3)); // Output: 27
+
+//Question-6
+var sub = (a,b)=>{
+    return a-b;
+}
+console.log(sub(5,1))
+
+//Question-7
+var n = ()=>{
+    console.log("Hello,world!");
+}
+n();
+
+
+
---
+++ day1.py
@@ -0,0 +1,7 @@
+num=2
+if(num>0):
+    print("positive")
+elif (num==0):
+    print("Zero")
+elif(num<0):
+    print("Negative")
