e4a3da73dacf3ca33510b894cda9499a89cfc0f8
Abhiramsai22 <narayandaduabhiramsai@gmail.com>
2025-04-08 14:58:36 +0530

    v1

---
+++ Js-error/Strings_task/index.js
@@ -0,0 +1,38 @@
+
+let str = "Hello";
+console.log(str.length);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+console.log(str.charAt(1));
+
+console.log(str.indexOf("l"));
+
+console.log(str.lastIndexOf("l"));
+
+console.log(str.includes("ell"));
+
+console.log(str.startsWith("he"));
+console.log(str.endsWith("lo")); 
+
+console.log(str.substring(1, 4));
+
+console.log(str.slice(1, 4)); 
+console.log(str.slice(-3));   
+
+console.log("  hello  ".trim());
+
+console.log(str.replace("world", "JS"));
+
+console.log("a,b,c".split(",")); 
+
+console.log("hello".concat(" world"));
+
+console.log("ha".repeat(3));
+
+console.log("abc123".match(/\d+/));
+
+
+
+
